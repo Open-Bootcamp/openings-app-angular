caf27b600d7ccb9230284cc7b2f606f5b7e86adc
Raphy Mejia <moyeto3@gmail.com>
2022-09-10 16:44:36 -0700

    Data service and anime type done

---
+++ src/app/data/data.ts
@@ -0,0 +1,140 @@
+export const data = [
+  {
+    id: "1",
+    name: "Shingeki no Kyojin",
+    opName: "Guren No Yumiya",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/snk-banner_fzs10s.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1659890401/anime-imgs/Shingeki_no_Kyojin_Guren_no_Yumiya_OP_1_sub_espa%C3%B1ol_lyrics_letra_dkrqex.mp3",
+    openings: [
+      {
+        title: "Guren no Yumiya",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659890401/anime-imgs/Shingeki_no_Kyojin_Guren_no_Yumiya_OP_1_sub_espa%C3%B1ol_lyrics_letra_dkrqex.mp3",
+      },
+      {
+        title: "Shinzou wo Sasageyo",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659924917/anime-imgs/Shingeki_No_Kyojin_2_Shinzou_wo_Sasageyo_ydagip.mp3",
+      },
+      {
+        title: "The Rumbling",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659925423/anime-imgs/Shingeki_No_Kyojin_OP7_The_Rumbling_pabwzw.mp3",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "Death Note",
+    opName: "The World",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/deathnote-banner_ehtkig.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1659890770/anime-imgs/The_World_Death_Note_OP1_uywkx1.mp3",
+    openings: [
+      {
+        title: "The World",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659890770/anime-imgs/The_World_Death_Note_OP1_uywkx1.mp3",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "Tokyo Revengers",
+    opName: "Crybaby",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/tokyo-banner_co9wwt.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1659890140/anime-imgs/Tokyo_Revengers_Opening_Cry_Baby_elh6oj.mp3",
+    openings: [
+      {
+        title: "Crybaby",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659890140/anime-imgs/Tokyo_Revengers_Opening_Cry_Baby_elh6oj.mp3",
+      },
+    ],
+  },
+  {
+    id: "4",
+    name: "Jujutsu Kaisen",
+    opName: "Kaikai Kitan",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659840619/anime-imgs/jk-banner_sd3z8i.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1659899650/anime-imgs/%E5%BB%BB%E5%BB%BB%E5%A5%87%E8%AD%9A_Eve_MV_swktzu.mp3",
+    openings: [
+      {
+        title: "Kaikai Kitan",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659899650/anime-imgs/%E5%BB%BB%E5%BB%BB%E5%A5%87%E8%AD%9A_Eve_MV_swktzu.mp3",
+      },
+    ],
+  },
+  {
+    id: "5",
+    name: "Tokyo Ghoul",
+    opName: "Unravel",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659840619/anime-imgs/tg-banner_kx24xr.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1659899656/anime-imgs/Unravel_q4yaga.mp3",
+    openings: [
+      {
+        title: "Unravel",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659899656/anime-imgs/Unravel_q4yaga.mp3",
+      },
+    ],
+  },
+  {
+    id: "6",
+    name: "Kimetsu no Yaiba",
+    opName: "Gurenge",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/kny-banner_j1y077.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1659899654/anime-imgs/Demon_Slayer_Opening_1_xvxwyc.mp3",
+    openings: [
+      {
+        title: "Gurenge",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659899654/anime-imgs/Demon_Slayer_Opening_1_xvxwyc.mp3",
+      },
+    ],
+  },
+  {
+    id: "7",
+    name: "Full Metal Alchemist",
+    opName: "Again",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659922724/anime-imgs/Fullmetal-Alchemistjpg_rekmhj.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1659922295/anime-imgs/Again_OP1_Subtitulado_Al_Espa%C3%B1ol_Fullmetal_Alchemist_Brotherhood_aqaxdq.mp3",
+    openings: [
+      {
+        title: "Again",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1659922295/anime-imgs/Again_OP1_Subtitulado_Al_Espa%C3%B1ol_Fullmetal_Alchemist_Brotherhood_aqaxdq.mp3",
+      },
+    ],
+  },
+  {
+    id: "8",
+    name: "One Punch Man",
+    opName: "The Hero!",
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1660015403/anime-imgs/onepunchman_odk5nt.webp",
+    soundSrc:
+      "https://res.cloudinary.com/daobmfotr/video/upload/v1660016224/anime-imgs/One_Punch_Man_Opening_OP_1080p_qwyzs8.mp3",
+    openings: [
+      {
+        title: "The Hero!",
+        "op":
+          "https://res.cloudinary.com/daobmfotr/video/upload/v1660016224/anime-imgs/One_Punch_Man_Opening_OP_1080p_qwyzs8.mp3",
+      },
+    ],
+  },
+];
